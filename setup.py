cff4c5561c67c21ae39ae3f778ee3b3645ad2a28
Gediminas <gedisu@gmail.com>
2017-05-16 09:37:51 +0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='DeepSecurity.py',
+    version='v.2.1.2',
+    packages=['deepsecurity',],
+    license='MIT',
+    long_description=open('README.md').read(),
+)
